df3fe4580eb20f9e40b93f1309836fb80736fc3a
anthonynarine <anarine83@gmail.com>
2023-10-11 10:41:16 -0400

    updated accessing Project SECRET_KEY + OPEN_API_KEY decouple did not work but dotenv does

---
+++ backend/core/urls.py
@@ -0,0 +1,10 @@
+
+# project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/', include('chatbot.urls')), 
+]
